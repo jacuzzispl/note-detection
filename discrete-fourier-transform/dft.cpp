a84071b43a1f895882c30abd1a11630c2b0c3776
David <atunwadavidb@gmail.com>
2026-04-27 21:06:27 +0100

    haha no way it actually works

--- discrete-fourier-transform/dft.cpp
+++ discrete-fourier-transform/dft.cpp
@@ -3,26 +3,35 @@
 #include <complex>
 #include <cmath>
 
-void DFT(const std::vector<double> &samples, int rate)
-{
-    double freqResolution = rate / samples.size();
+using namespace std;
 
+void DFT(const std::vector<double> samples, int rate)
+{
     const double pi = std::acos(-1.0);
 
-    const std::complex<double> i(0.0, 1.0);
+    std::vector<std::complex<double>> output(samples.size(), std::complex<double>(0.0, 0.0));
 
-    std::vector<std::complex<double>> output(0, samples.size());
+    // k = i (the frequency index)
+    // n = j (the index of the current sample)
+    // N = samples.size() (the number of samples)
 
-    for (int i = 0; i <= samples.size(); i++)
+    for (int i = 0; i < samples.size(); i++) // this first for loop is the frequencies
     {
-        for (int j = 0; j <= samples.size(); i++)
+        double theta = (-2 * pi * i) / samples.size();
+
+        for (int j = 0; j < samples.size(); j++) // why do we need this second for loop?
         {
             // multiplying the current xn with the frequency wave
 
             // turning real number from sample into complex number
-            output[i] += static_cast<std::complex<double>>(samples[i], 0.0) * std::exp(-i * ((2 * pi) / samples.size()) * i);
-            std::cout << output[i];
+            // output.at(i) += static_cast<std::complex<double>>(samples[i], 0.0) * std::exp(-i * ((2 * pi) / samples.size()) * i);
+            // std::cout << static_cast<std::complex<double>>(samples[i]) * std::exp(-i * ((2 * pi) / samples.size()) * j) << endl;
+
+            std::complex<double> weight(cos(theta * j), sin(theta * j));
+
+            output.at(i) += samples.at(j) * weight;
         }
+        std::cout << "Index " << i << " : " << output[i] << endl;
     }
 }
 
@@ -30,7 +39,5 @@ int main()
 {
     std::vector<double> input = {0.16, -0.32, 0.88, 0.77, -0.39};
 
-    std::vector<double> &ref = input;
-
-    DFT(ref, 2);
+    DFT(input, 2);
 }
